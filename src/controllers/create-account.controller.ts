bd7839bf42fabec2890bee339163cd3e4cb1c16d
Gabrielcoutz <gabriel.coutinho.cassiano@gmail.com>
2026-03-15 17:52:56 -0300

    feat: add Zod validation pipe and bcrypt for password hashing in account creation

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,22 +1,49 @@
-import { Body, ConflictException, Controller, Post } from '@nestjs/common'
+import {
+  Body,
+  ConflictException,
+  Controller,
+  Post,
+  UsePipes,
+} from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
+import * as bcrypt from 'bcryptjs'
+import z from 'zod'
+import { ZodValidationPipe } from 'src/pipes/zod-validation.pipe'
+
+const createAccountBodySchema = z.object({
+  name: z.string().min(3, 'Name must be at least 3 characters long'),
+  email: z.email('Invalid email address'),
+  password: z.string().min(6, 'Password must be at least 6 characters'),
+})
+
+type CreateAccountBody = z.infer<typeof createAccountBodySchema>
 
 @Controller('/accounts')
 export class CreateAccountController {
   constructor(private prisma: PrismaService) {}
 
   @Post()
-  async handle(@Body() body) {
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(@Body() body: CreateAccountBody) {
+    const { name, email, password } = body
+
     const emailAlreadyInUse = await this.prisma.user.findUnique({
       where: {
-        email: body.email,
+        email,
       },
     })
 
     if (emailAlreadyInUse) throw new ConflictException('Email already in use')
 
-    const user = await this.prisma.user.create({
-      data: body,
+    const hashedPassword = await bcrypt.hash(password, 8)
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password: _, ...user } = await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
     })
 
     return { user }
