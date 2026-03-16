6fb7c6330b909ce792dfdadfbbb6030d3090b669
Gabrielcoutz <gabriel.coutinho.cassiano@gmail.com>
2026-03-16 19:25:54 -0300

    feat: move HTTP controllers and Prisma service to a new HttpModule for better organization

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,23 +1,14 @@
 import { AuthModule } from '@/infra/auth/auth.module'
 import { envSchema } from '@/infra/env'
-import { AuthenticateController } from '@/infra/http/controllers/authenticate.controller'
-import { CreateAccountController } from '@/infra/http/controllers/create-account.controller'
-import { CreateQuestionController } from '@/infra/http/controllers/create-question.controller'
-import { FetchRecentQuestionsController } from '@/infra/http/controllers/fetch-recent-questions.controller'
-import { PrismaService } from '@/infra/prisma/prisma.service'
+import { HttpModule } from '@/infra/http/http.module'
+
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  controllers: [
-    CreateAccountController,
-    AuthenticateController,
-    CreateQuestionController,
-    FetchRecentQuestionsController,
-  ],
-  providers: [PrismaService],
   imports: [
     AuthModule,
+    HttpModule,
     ConfigModule.forRoot({
       expandVariables: true,
       isGlobal: true,
